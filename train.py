14bea640c1e1dbc7d9db855dd195a5cc7a65dd15
dat <dat.nc200137@sis.hust.edu.vn>
2024-04-05 21:29:20 +0700

    init commit

--- train.py
+++ train.py
@@ -129,7 +129,7 @@ def train(params: argparse.Namespace):
                 b = img.shape[0]
                 optimizer.zero_grad()
                 x_0 = img.to(device)
-                lab = kwargs["y"].type(torch.FloatTensor).to(device)
+                lab = kwargs["y"].type(torch.int8).to(device)
 
                 cemb = cemblayer(lab)
                 cemb[np.where(np.random.rand(b) < params.threshold)] = 0
